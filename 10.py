9fb43173c90ed543fcdbcb8653c9eff227d28cf7
Feng LYU <aragon1130@gmail.com>
2016-09-02 16:50:03 +0800

    finish problem 10 with c and python, compare the speed

--- 10.py
+++ 10.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 
+def prime(n):
+    i = 2;
+    if n <= 1:
+        return False
+
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i = i + 1
+    return True
 
 def main():
 
+    sum = 2
+    for i in range(3, 2000001, 2):
+        if prime(i):
+#            print(i)
+            sum += i
+#        print(sum)
+
+    print("result is %d\n" % sum)
 
 if __name__ == "__main__":
     main()
+
