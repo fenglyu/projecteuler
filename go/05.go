32d7cec7d2dc737d19bfbfc3e8a40c35861fd8b3
flv <flv@blizzard.com>
2019-09-16 13:57:53 +0800

    update

--- go/05.go
+++ go/05.go
@@ -8,30 +8,53 @@ import (
 
 func GetPrimes(n int) []int {
 
-	i := 1
-
-	s := make([]int, 21)
-	for {
-
-		if n == 1 || n == 2 {
-			s = append(s, n)
-			return s
+	s := make([]int, n+1)
+	for i := 2; i*i <= n; i++ {
+		for {
+			if n%i == 0 {
+				s[i] += 1
+				n = int(n / i)
+			} else {
+				break
+			}
 		}
-
-		if i*i > n {
-			break
-		}
-
-		if n%i == 0 {
-			s = append(s, i, n/i)
-		}
-
-		i += 1
 	}
 
+	if n > 1 {
+		s[n] += 1
+	}
+
+	//	fmt.Println(s)
 	return s
 }
 
 func main() {
-	fmt.Println(GetPrimes(10))
+	//fmt.Println(GetPrimes(10))
+
+	s := make([]int, 21)
+
+	for i := 1; i <= 20; i++ {
+		//		p := GetPrimes(i)
+		//		fmt.Println(p)
+		for j, x := range GetPrimes(i) {
+			if x > s[j] {
+				s[j] = x
+			}
+		}
+	}
+
+	sum := 1
+
+	for i, v := range s {
+		for {
+			if v <= 0 {
+				break
+			}
+
+			sum = sum * i
+			v -= 1
+		}
+	}
+	//fmt.Println(s)
+	fmt.Println(sum)
 }
