8405ab0c2a958e86c6ccea5da23742ef72ec94d2
flv <flv@blizzard.com>
2019-09-17 18:05:17 +0800

    Update project euler

--- go/12.go
+++ go/12.go
@@ -4,7 +4,38 @@ import (
 	"fmt"
 )
 
-func main() {
-	//	fmt.Printf("sum: %d\n", sum)
+func divisor(n int) int {
 
+	num, i := 0, 1
+	for {
+		if i*i > n {
+			break
+		}
+
+		if n%i == 0 {
+			num += 2
+		}
+		i += 1
+	}
+
+	if i*i == n {
+		num -= 1
+	}
+
+	return num
+}
+
+func main() {
+
+	i := 1
+	sum := 0
+	for {
+		sum = divisor(i * (i + 1) / 2)
+		if sum >= 500 {
+
+			fmt.Printf("sum: %d, num: %d\n", sum, i*(i+1)/2)
+			break
+		}
+		i++
+	}
 }
