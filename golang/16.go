7139c162ba1f14feb5c9217a17018a45fa89ce14
flv <flv@blizzard.com>
2019-09-20 12:24:47 +0800

    update

--- golang/16.go
+++ golang/16.go
@@ -6,26 +6,37 @@ import (
 
 func mul(a interface{}, b interface{}) []int {
 
-	c := []int{2, 3, 4, 5}
-
 	f := a.([]int)
 	g := b.([]int)
-	fmt.Println("f: ", f, "g: ", g)
 
-	result := make([]int, (len(f)+len(g))*2)
-
-	d := 0
-	for i, v := range f {
-		for j, p := range g {
-			//			result[i] = v + p
-			result[i+j] = v + p
-			d++
-		}
+	if len(f) < len(g) {
+		t := f
+		f = g
+		g = t
 	}
 
-	fmt.Println(result)
+	//fmt.Println("f: ", f, "g: ", g)
+	result := make([]int, (len(f)+len(g))*2)
 
-	return c
+	for j := len(g) - 1; j >= 0; j-- {
+		//initially pos (index of result ) has the same relative postion as j in array
+		pos := len(result) - (len(g) - j)
+		for i := len(f) - 1; i >= 0; i-- {
+			temp := g[j] * f[i]
+			result[pos] += temp % 10
+			result[pos-1] += temp / 10
+			pos--
+		}
+		//	fmt.Println(result)
+	}
+
+	for i := len(result) - 1; i > 0; i-- {
+		temp := result[i]
+		result[i] = temp % 10
+		result[i-1] += temp / 10
+	}
+
+	return result
 }
 
 func main() {
@@ -33,5 +44,64 @@ func main() {
 	b := []int{4, 5, 3, 1}
 
 	result := mul(a, b)
-	fmt.Println(result)
+	//	fmt.Println(result)
+
+	sum := 0
+	inc := 1
+	for i := len(result) - 1; i > 0; i-- {
+		sum += result[i] * inc
+		inc = inc * 10
+	}
+	fmt.Println(sum)
+
+	// 2**50
+	c := []int{1, 1, 2, 5, 8, 9, 9, 9, 0, 6, 8, 4, 2, 6, 2, 4}
+	d := []int{1, 1, 2, 5, 8, 9, 9, 9, 0, 6, 8, 4, 2, 6, 2, 4}
+	res := make([]int, 1000)
+	res[999] = 1
+
+	//	for i := 0; i < 20; i++ {
+	//		res = mul(res, c)
+	//		//		if (len(c) + len(res) + 1) > cap(res) {
+	//		//			t := make([]int, len(res), (cap(res)+1)*2)
+	//		//			copy(t, res)
+	//		//			res = t
+	//		//		}
+	//	}
+
+	res = mul(d, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	res = mul(res, c)
+	// get it
+	//fmt.Println(res)
+	sum = 0
+	for i := len(res) - 1; i >= 0; i-- {
+		sum += res[i]
+	}
+	fmt.Println(sum)
+
 }
+
+/*
+result same as python3 result 2**1000
+
+[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 7 1 5 0 8 6 0 7 1 8 6 2 6 7 3 2 0 9 4 8 4 2 5 0 4 9 0 6 0 0 0 1 8 1 0 5 6 1 4 0 4 8 1 1 7 0 5 5 3 3 6 0 7 4 4 3 7 5 0 3 8 8 3 7 0 3 5 1 0 5 1 1 2 4 9 3 6 1 2 2 4 9 3 1 9 8 3 7 8 8 1 5 6 9 5 8
+5 8 1 2 7 5 9 4 6 7 2 9 1 7 5 5 3 1 4 6 8 2 5 1 8 7 1 4 5 2 8 5 6 9 2 3 1 4 0 4 3 5 9 8 4 5 7 7 5 7 4 6 9 8 5 7 4 8 0 3 9 3 4 5 6 7 7 7 4 8 2 4 2 3 0 9 8 5 4 2 1 0 7 4 6 0 5 0 6 2 3 7 1 1 4 1 8 7 7 9 5 4 1 8 2 1 5 3 0 4 6 4 7 4 9 8 3 5 8 1 9 4 1 2 6 7 3 9 8 7 6 7 5 5 9 1 6 5 5 4 3 9 4 6 0 7 7 0 6 2 9 1 4 5 7 1 1 9 6 4 7 7 6 8 6 5 4 2 1 6 7 6 6 0 4 2 9 8 3 1 6 5 2 6 2 4 3 8 6 8 3 7 2 0 5 6 6 8 0 6 9 3 7 6]
+*/
