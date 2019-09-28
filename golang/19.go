d888169b79cf78324f7edc9e795f342f4e89ac9a
Feng LYU <aragon1130@gmail.com>
2019-09-29 00:07:40 +0800

    finish weekday caculation

--- golang/19.go
+++ golang/19.go
@@ -16,22 +16,24 @@ const (
 	Sat
 )
 
+var MonNum = [2][13]int{
+	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+	{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}}
+
+var WeekNum = [7]Week{Sun, Mon, Tue, Wed, Thur, Fri, Sat}
+
 type Leap struct {
 	days int
 	leap bool
 }
 
-//func (l Leap) ToString() string {
-//	return fmt.Sprintf("Leap:[ %d %v]", l.days, l.leap)
-//}
-//
-//func (l Leap) String() string {
-//	return fmt.Sprintf("Leap: %d %v", l.days, l.leap)
-//}
-
 type Day struct {
 	dom  int // day of week
 	week Week
+}
+
+func (d *Day) setWeek(w Week) {
+	d.week = w
 }
 
 type Month struct {
@@ -39,9 +41,28 @@ type Month struct {
 	num int // number of days
 }
 
+func (m *Month) setMonth(leap bool) {
+	i := 0
+	if leap {
+		i = 1
+	}
+	m.num = MonNum[i][m.m]
+}
+
 type Year struct {
 	y    int  // year number, e.g. 1990
 	leap bool // leap or not
+}
+
+func (y *Year) setLeap() {
+	y.leap = isLeap(y.y).leap
+}
+
+func (y Year) getDays() int {
+	if y.leap {
+		return 366
+	}
+	return 365
 }
 
 type Date struct {
@@ -51,13 +72,37 @@ type Date struct {
 	//	Week
 }
 
-func (d *Date) CountDays(w Week, t *Date) {
-	y := d.Year.y - t.Year.y
-	m := d.Month.m - t.Month.m
-	day := d.Day.dom - t.Day.dom
-	fmt.Println(y)
-	fmt.Println(m)
-	fmt.Println(day)
+func (d *Date) CountDays() int {
+	leap := 0
+	if d.leap {
+		leap = 1
+	}
+
+	days := 0
+	for i := 1; i < d.Month.m; i++ {
+		days += MonNum[leap][i]
+	}
+
+	return days + d.Day.dom
+}
+
+func (d *Date) DiffYearsInDays(t *Date) int {
+
+	yd := 0
+	for i := t.Year.y; i < d.Year.y; i++ {
+		yd += isLeap(i).days
+	}
+
+	yd += d.CountDays() - t.CountDays()
+	return yd
+}
+
+func (d *Date) GetWeekOfDay(s *Date, w Week) Week {
+	//fmt.Println(d.DiffYearsInDays(t))
+	diff_days := d.DiffYearsInDays(s)
+	pos := diff_days % 7
+	sw := int(w)
+	return Week((sw + pos) % 7)
 }
 
 func isLeap(n int) Leap {
@@ -79,8 +124,8 @@ func main() {
 
 	d := &Date{
 		Year: Year{
-			y:    1992,
-			leap: true,
+			y: 1992,
+			//		leap: true,
 		},
 		Month: Month{
 			m:   2,
@@ -91,11 +136,12 @@ func main() {
 			week: Tue,
 		},
 	}
+	d.Year.setLeap()
 
 	t := &Date{
 		Year: Year{
-			y:    2041,
-			leap: false,
+			y: 2041,
+			//		leap: false,
 		},
 		Month: Month{
 			m:   11,
@@ -107,21 +153,43 @@ func main() {
 		},
 	}
 
+	t.Year.setLeap()
+
 	f := &Date{
 		Year: Year{
-			y:    2019,
-			leap: false,
+			y: 2018,
+			//			leap: false,
 		},
 		Month: Month{
-			m:   9,
-			num: 30,
+			m:   2,
+			num: 28,
 		},
 		Day: Day{
-			dom:  26,
-			week: Thur,
+			dom: 21,
+			//			week: Thur,
 		},
 	}
 
-	t.CountDays(Tue, d)
-	f.CountDays(Thur, t)
+	f.Year.setLeap()
+
+	g := &Date{
+		Year: Year{
+			y: 2019,
+			//			leap: false,
+		},
+		Month: Month{
+			m: 9,
+			//			num: 30,
+		},
+		Day: Day{
+			dom: 29,
+			//	week: Thur,
+		},
+	}
+
+	f.Year.setLeap()
+	fmt.Println(t.CountDays())
+	fmt.Println(f.CountDays())
+	fmt.Println(f.DiffYearsInDays(t))
+	fmt.Println(g.GetWeekOfDay(f, Wed))
 }
