ffefd27ec0a6f018e17ced69b9982f6b6b239dbd
Feng LYU <aragon1130@gmail.com>
2019-10-04 21:14:48 +0800

    update 20

--- golang/20.go
+++ golang/20.go
@@ -39,7 +39,7 @@ func mul(a interface{}, b interface{}) []int {
 	return result
 }
 
-func main() {
+func solution1() {
 
 	res := make([]int, 1000)
 	res[999] = 1
@@ -70,4 +70,72 @@ func main() {
 	}
 
 	fmt.Println(sum)
+}
+
+func mul2(a []int, b []int, r []int) { //[]int {
+
+	pos := len(r) - 1
+
+	for i := 0; i < len(r)-1; i++ {
+		r[i] = 0
+	}
+
+	for i := len(a) - 1; i >= 0; i-- {
+		off := len(a) - 1 - i
+
+		for j := len(b) - 1; j >= 0; j-- {
+			fmt.Println(pos, off, pos-off, pos-off-1)
+			tmp := b[j] * a[i]
+			r[pos-off] += tmp % 10
+			r[pos-off-1] += r[pos-off]/10 + tmp/10
+			r[pos-off] %= 10
+			off++
+		}
+	}
+
+	//	return r
+}
+
+func solution2() {
+
+	r := make([]int, 1000)
+
+	res := make([]int, 1000)
+	res[999] = 1
+
+	for i := 1; i < 101; i++ {
+
+		ar := []int{0, 0, 0}
+		count, c := len(ar)-1, i
+
+		for {
+			if c == 0 || count < 0 {
+				break
+			}
+
+			ar[count] = c % 10
+
+			c = c / 10
+			count--
+		}
+		//res = mul2(res, ar, res)
+		mul2(ar, res, r)
+
+		res = r
+	}
+
+	fmt.Println(res)
+
+	sum := 0
+	for _, v := range res {
+		sum += v
+	}
+
+	fmt.Println(sum)
+}
+
+func main() {
+
+	solution1()
+	solution2()
 }
