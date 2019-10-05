0a9e15ac0b47b49c650731b551b6739f94a70383
Feng LYU <aragon1130@gmail.com>
2019-10-06 03:19:36 +0800

    update 21

--- golang/21.go
+++ golang/21.go
@@ -15,23 +15,80 @@ import (
 
 func divisor(n int) []int {
 
-	num, i := 0, 1
+	nums := make([]int, 500)
+
+	for i := 0; i < len(nums); i++ {
+		nums[i] = 0
+	}
+
+	i, c := 1, 0
+
 	for {
 		if i*i > n {
 			break
 		}
 
-		if n%i == 0 {
-			num += 2
+		tmp := n % i
+		if tmp == 0 {
+			nums[c] = i
+			// only collect 1, ingore the number itself
+			if i != 1 && i != n/i {
+				c++
+				nums[c] = n / i
+			}
 		}
-		i += 1
+		i++
+		c++
+		//fmt.Println("c:  ", c)
 	}
 
-	return num
+	return nums
+}
+
+func sum1j(ar []int) int {
+
+	s := 0
+	for _, v := range ar {
+		s += v
+	}
+	return s
 }
 
 func main() {
-	c, sum := 1, 0
+	//	sum := 40
+	//
+	//	nums := divisor(sum)
+	//	fmt.Println("sum: ", nums)
+	//	// 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110;
+	//	fmt.Println("220: ", divisor(220))
+	//
+	//	s := 0
+	//	for _, v := range divisor(220) {
+	//		s += v
+	//	}
+	//	fmt.Println("220: ", s)
+	//	// 1, 2, 4, 71 and 142
+	//	fmt.Println("284: ", divisor(284))
+	//s = 0
+	//for _, v := range divisor(284) {
+	//	s += v
+	//}
+	//fmt.Println("284: ", s)
 
-	fmt.Printf("sum: %d\n", sum)
+	btk := make([]int, 10001)
+	ar := make([]int, 100)
+
+	for i := 1; i < 10001; i++ {
+		tf := divisor(i)
+		f := sum1j(tf)
+		ts := divisor(f)
+		s := sum1j(ts)
+		//fmt.Println(i, tf, ts, s)
+		if f == s {
+			ar = append(ar, f, s)
+		}
+		//	fmt.Println(ar)
+	}
+
+	fmt.Println(ar)
 }
