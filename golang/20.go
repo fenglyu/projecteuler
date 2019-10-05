f46f38618954686511805f121aa3e906f57f37be
Feng LYU <aragon1130@gmail.com>
2019-10-06 01:20:35 +0800

    update 20 with better algorimths

--- golang/20.go
+++ golang/20.go
@@ -76,7 +76,7 @@ func mul2(a []int, b []int, r []int) { //[]int {
 
 	pos := len(r) - 1
 
-	for i := 0; i < len(r)-1; i++ {
+	for i := 0; i < len(r); i++ {
 		r[i] = 0
 	}
 
@@ -84,10 +84,12 @@ func mul2(a []int, b []int, r []int) { //[]int {
 		off := len(a) - 1 - i
 
 		for j := len(b) - 1; j >= 0; j-- {
-			fmt.Println(pos, off, pos-off, pos-off-1)
+			//			fmt.Println(i, j, pos, off, pos-off, pos-off-1)
 			tmp := b[j] * a[i]
 			r[pos-off] += tmp % 10
+			//	if pos-off-1 > 0 {
 			r[pos-off-1] += r[pos-off]/10 + tmp/10
+			//	}
 			r[pos-off] %= 10
 			off++
 		}
@@ -100,8 +102,8 @@ func solution2() {
 
 	r := make([]int, 1000)
 
-	res := make([]int, 1000)
-	res[999] = 1
+	res := make([]int, 200)
+	res[199] = 1
 
 	for i := 1; i < 101; i++ {
 
@@ -118,16 +120,18 @@ func solution2() {
 			c = c / 10
 			count--
 		}
-		//res = mul2(res, ar, res)
-		mul2(ar, res, r)
 
-		res = r
+		mul2(res, ar, r)
+		//fmt.Println(res[190:], ar, r[990:])
+		for j := 0; j < len(res); j++ {
+			res[j] = r[len(r)-len(res)+j]
+		}
 	}
 
-	fmt.Println(res)
+	//	fmt.Println(r)
 
 	sum := 0
-	for _, v := range res {
+	for _, v := range r {
 		sum += v
 	}
 
@@ -138,4 +142,5 @@ func main() {
 
 	solution1()
 	solution2()
+
 }
