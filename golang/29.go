2f77a4b170ae629636390d1d7735a83d40c8577d
Feng Lyu <fenglyu007@gmail.com>
2019-12-10 00:16:33 +0800

    update

--- golang/29.go
+++ golang/29.go
@@ -21,15 +21,22 @@ import (
 func main() {
 	//	ar := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}
 
+	m := make(map[int][]int)
 	for i := 2; i < 101; i++ {
 		res := common.DivisorV3(i)
 		if len(res) < 2 {
 			continue
 		}
-
-		fmt.Println("num: ", i, res)
+		//	fmt.Println("num: ", i, res)
+		m[i] = res
 	}
 
+	//	fmt.Println("map: ", m)
+	for j := 2; j <= 10; j++ {
+		for k, v := range m {
+
+		}
+	}
 }
 
 /*
